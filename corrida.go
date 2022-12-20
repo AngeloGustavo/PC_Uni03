3f8c035833afe4d6150ae6d09506ab6977c1e1e1
AngeloGustavo <angelogustavo157@gmail.com>
2022-12-20 01:46:52 -0300

    feat: Anuncio do vencedor

--- corrida.go
+++ corrida.go
@@ -7,15 +7,25 @@ import (
 
 var wg sync.WaitGroup
 
-func corredor(raia chan int, name string) {
-	fmt.Println("Corredor da ", name, "esta pronto para receber o bastao")
+func corredor(raia chan int, name string, vencedor *string) {
 	bastao := <-raia
-	fmt.Println("Corredor ", bastao, "da ", name, "esta correndo")
+
+	if bastao == 1 {
+		fmt.Println("Equipe", name, "está preparada")
+	} else {
+		fmt.Println("Corredor", bastao, "da equipe", name, "está pronto para receber o bastão")
+	}
+
+	fmt.Println("Corredor", bastao, "da equipe", name, "esta correndo")
 	if bastao == 4 {
+		fmt.Println("\\o/ Equipe", name, "terminou a corrida!")
+		if *vencedor == "" {
+			*vencedor = name
+		}
 		wg.Done()
 		return
 	}
-	go corredor(raia, name)
+	go corredor(raia, name, vencedor)
 
 	bastao++
 
@@ -23,6 +33,7 @@ func corredor(raia chan int, name string) {
 }
 
 func main() {
+	vencedor := ""
 	wg.Add(4)
 
 	raia := make(chan int)
@@ -31,26 +42,26 @@ func main() {
 	raia3 := make(chan int)
 
 	go func() {
-		go corredor(raia, "Equipe bola")
+		go corredor(raia, "Bola", &vencedor)
 
 		raia <- 1
 
 	}()
 	go func() {
 
-		go corredor(raia1, "Equipe triangulo")
+		go corredor(raia1, "Triângulo", &vencedor)
 
 		raia1 <- 1
 	}()
 	go func() {
-		go corredor(raia2, "Equipe quadrado")
+		go corredor(raia2, "Quadrado", &vencedor)
 
 		raia2 <- 1
 
 	}()
 	go func() {
 
-		go corredor(raia3, "Equipe losango")
+		go corredor(raia3, "Losango", &vencedor)
 
 		raia3 <- 1
 	}()
@@ -58,4 +69,7 @@ func main() {
 	wg.Wait()
 
 	fmt.Println("A corrida acabou!")
+	fmt.Println()
+	fmt.Println("===== Equipe", vencedor, "é a grande campeã!!! =====")
+	fmt.Println()
 }
