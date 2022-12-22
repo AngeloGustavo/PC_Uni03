3bd2aa82be211c45fca8cba29626d7fc7d2c6962
AngeloGustavo <angelogustavo157@gmail.com>
2022-12-22 01:42:36 -0300

    feat: Comentado

--- corrida.go
+++ corrida.go
@@ -5,34 +5,45 @@ import (
 	"sync"
 )
 
+// notifica o programa para esperar por uma ação
 var wg sync.WaitGroup
 
-func corredor(raia chan int, name string, vencedor *string) {
+/*
+Função usada por cada umas das 4 threads para
+@param raia raia da equipe
+@param nome nome da equipe
+@param vencedor ponteiro que no fim da corrida recebe o nome da equipe vencedora
+*/
+func corredor(raia chan int, nome string, vencedor *string) {
 	bastao := <-raia
 
 	if bastao == 1 {
-		fmt.Println("Equipe", name, "está preparada")
+		fmt.Println("Equipe", nome, "está preparada")
 	} else {
-		fmt.Println("Corredor", bastao, "da equipe", name, "está pronto para receber o bastão")
+		fmt.Println("Corredor", bastao, "da equipe", nome, "está pronto para receber o bastão")
 	}
 
-	fmt.Println("Corredor", bastao, "da equipe", name, "esta correndo")
+	fmt.Println("Corredor", bastao, "da equipe", nome, "esta correndo")
 	if bastao == 4 {
-		fmt.Println("\\o/ Equipe", name, "terminou a corrida!")
+		fmt.Println("\\o/ Equipe", nome, "terminou a corrida!")
 		if *vencedor == "" {
-			*vencedor = name
+			*vencedor = nome
 		}
 		wg.Done()
 		return
 	}
-	go corredor(raia, name, vencedor)
+	go corredor(raia, nome, vencedor)
 
 	bastao++
 
 	raia <- bastao
 }
 
+/*
+Função main executável
+*/
 func main() {
+	// string que recebe o nome da equipe vencedora
 	vencedor := ""
 	wg.Add(4)
 
